60ee2cecd515f5301a76c74e6295cf2cdf2c422f
morgan edwards <medwardsii@gmail.com>
2017-12-31 16:39:16 -0500

    User signup, login, logout works on the backend. API utils for session in place

--- frontend/soundmound.jsx
+++ frontend/soundmound.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Testing imports
+import * as API from './util/session_api_util'
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  //Testing assignments
+  window.login = API.login;
+  window.logout = API.logout;
+  window.signup = API.signup;
+  window.testboy = {username: 'testboy', password: 'testboy'};
+  //End of testing assignments
   ReactDOM.render(<h1>Soundmound</h1>, root);
 });
